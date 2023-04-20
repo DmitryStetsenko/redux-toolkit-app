75f7d8fd89287524e91af33fd8d0a779dabbfbc4
Stetsenko Dmitry <ebusines2010@gmail.com>
2023-04-20 11:08:49 +0300

    base stucture and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,48 @@
+import { useState } from "react";
+
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [text, setText] = useState('');
+
+  const addTodo = () => {
+    if (!text.trim()) {
+      return;
+    }
+
+    setTodos(
+      [...todos, { id: new Date().toISOString(), text, completed: false}]
+    );
+    setText('');
+  }
+
   return (
     <div className="App">
+
       <h1>Redux toolkit</h1>
+
+      <div className="addTodo">
+        <label className="addTodo__label">
+          <input className="addTodo__input" type="text" value={ text } onChange={ e => setText(e.target.value) }/>
+        </label>
+        <button onClick={addTodo}>Add Todo</button>
+      </div>
+
+      <div className="todoListBlock">
+        <h2>Todo list</h2>
+        <ul className="todoList">
+          {
+            todos.map((todo, index) => (
+              <li key={todo.id} className="todoItem">
+                <span className="todoItem__number">{ index + 1 }</span>
+                <input className="todoItem__completed" type="checkbox" name="completed" checked={ todo.completed ? true : false } />
+                <p className="todoItem__text">{ todo.text }</p>
+                <span className="todoItem__del">&times;</span>
+              </li>
+            ))
+          }
+        </ul>
+      </div>
+      
     </div>
   );
 }
