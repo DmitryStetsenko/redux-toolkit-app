3032bf79e3fcc6b0602cc8ca8fcd17d4e4bc2346
Stetsenko Dmitry <ebusines2010@gmail.com>
2023-04-22 16:58:23 +0300

    crate fetchTodos

--- src/store/todoSlice.js
+++ src/store/todoSlice.js
@@ -1,4 +1,13 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+export const fetchTodos = createAsyncThunk(
+    'todos/fetchTodos',
+    async function() {
+        const response = await fetch('https://jsonplaceholder.typicode.com/todos');
+        const data = await response.json();
+        return data;
+    }
+);
 
 const todoSlice = createSlice({
     name: 'todos',
